c9f679049e32a6852fbf1e652dbe82c2b8ca65d9
phelisa <phelisajeruto@gmail.com>
2022-02-12 10:21:04 +0300

    created my first kotlinproject

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var fullName = "phelisia , Jeruto"
+    var age ="23"
+    var phoneNumber ="0718117097"
+    var weight  = "55kg"
+    val nationalityKenyan  : Boolean= true
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(nationalityKenyan)
+
+
+}
